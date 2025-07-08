2291f815757d8cc1476d71a043b9f887b0b77e10
diegosou4 <diegoaguia31@gmail.com>
2025-07-08 16:32:27 +0100

    (update) config done with tailwind

---
+++ my-app/src/app/index.tsx
@@ -0,0 +1,11 @@
+import {View,Text} from "react-native"
+
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">Welcome to the React Native App!</Text>
+    </View>
+  );
+}
